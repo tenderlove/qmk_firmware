90de0a113e7f1f44094cc2dd481c52dc65c36cdd
Aaron Patterson <aaron.patterson@gmail.com>
2016-10-01 17:28:28 -0700

    adding my layout

--- keyboards/ergodox/keymaps/tenderlove/keymap.c
+++ keyboards/ergodox/keymaps/tenderlove/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  |  L1  |           |  L1  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   D  |   F  |   K  |  Up  |           |  Up  |   J  |   U  |   R  |   L  |   ;  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   TG(SYMB),
+        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         KC_TAB,         KC_Q,         KC_W,   KC_D,   KC_F,   KC_K,   KC_UP,
         KC_LCTRL,       KC_A,         KC_S,   KC_E,   KC_T,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_DOWN,
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_HOME,
                                                KC_SPC,KC_ENT,KC_END,
         // right hand
-             TG(SYMB),    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
+             KC_RIGHT,    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_UP,       KC_J,   KC_U,   KC_R,   KC_L,   KC_SCLN,          KC_BSLS,
                           KC_Y,   KC_N,   KC_I,   KC_I,   KC_H,             KC_QUOT,
              KC_DOWN,     KC_P,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
