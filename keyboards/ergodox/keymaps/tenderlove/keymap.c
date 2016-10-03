7851e3fa8e6d831e79230ec5f34cac5757b0360e
Aaron Patterson <aaron.patterson@gmail.com>
2016-10-03 11:53:42 -0700

    updating stuff

--- keyboards/ergodox/keymaps/tenderlove/keymap.c
+++ keyboards/ergodox/keymaps/tenderlove/keymap.c
@@ -15,16 +15,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   D  |   F  |   K  |  Up  |           |  Up  |   J  |   U  |   R  |   L  |   ;  |   \    |
+ * | Tab    |   Q  |   W  |   D  |   F  |   K  |  =   |           |  Up  |   J  |   U  |   R  |   L  |   ;  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   E  |   T  |   G  |------|           |------|   Y  |   N  |   I  |   O  |   H  |   '    |
- * |--------+------+------+------+------+------| Down |           | Down |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  -   |           |  :   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   P  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Grv/L1|  '"  |   -  | LAlt | LGui |                                       | RGui | RAlt |   [  |   ]  | ~L1/-  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                  ,-----------------.       ,-----------------.
- *                                  | LGui+[ | LGui+] |       | LGui+[ | LGui+] |
+ *                                  | LEFT   | RIGHT  |       | LGui+[ | LGui+] |
  *                           ,------+--------+--------|       |--------+--------+------.
  *                           |      |        | Home   |       | PgUp   |        |      |
  *                           | Space|Enter   |------  |       |--------|  Ctrl/ |Back  |
@@ -36,18 +36,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,         KC_Q,         KC_W,   KC_D,   KC_F,   KC_K,   KC_UP,
+        KC_TAB,         KC_Q,         KC_W,   KC_D,   KC_F,   KC_K,   KC_EQL,
         KC_LCTRL,       KC_A,         KC_S,   KC_E,   KC_T,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_DOWN,
-        LT(SYMB,KC_GRV),KC_QUOT,      KC_MINS,KC_LALT,KC_LGUI,
-                                              LGUI(KC_LBRC),  LGUI(KC_RBRC),
-                                                              KC_HOME,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_MINS,
+        LT(SYMB,KC_GRV),KC_QUOT,      MO(MDIA),KC_LALT,KC_LGUI,
+                                                 KC_LEFT,  KC_RIGHT,
+                                                            KC_HOME,
                                                KC_SPC,KC_ENT,KC_END,
         // right hand
              KC_RIGHT,    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_UP,       KC_J,   KC_U,   KC_R,   KC_L,   KC_SCLN,          KC_BSLS,
                           KC_Y,   KC_N,   KC_I,   KC_O,   KC_H,             KC_QUOT,
-             KC_DOWN,     KC_P,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+             LSFT(KC_SCLN),KC_P,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                   KC_RGUI,KC_RALT,KC_LBRC,KC_RBRC,          LT(SYMB,KC_MINS),
 	     LGUI(KC_LBRC),  LGUI(KC_RBRC),
              KC_PGUP,
