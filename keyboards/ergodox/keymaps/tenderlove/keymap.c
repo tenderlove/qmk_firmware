779d342ac3f445bd6482d904996a7c372bb5d68a
Aaron Patterson <aaron.patterson@gmail.com>
2016-10-06 14:01:32 -0700

    ctrl/=

--- keyboards/ergodox/keymaps/tenderlove/keymap.c
+++ keyboards/ergodox/keymaps/tenderlove/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   D  |   F  |   K  |  =   |           |  Up  |   J  |   U  |   R  |   L  |   ;  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   E  |   T  |   G  |------|           |------|   Y  |   N  |   I  |   O  |   H  |   '    |
+ * | LCtrl/=|   A  |   S  |   E  |   T  |   G  |------|           |------|   Y  |   N  |   I  |   O  |   H  |   '    |
  * |--------+------+------+------+------+------|  -   |           |  :   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   P  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         KC_TAB,         KC_Q,         KC_W,   KC_D,   KC_F,   KC_K,   KC_EQL,
-        KC_LCTRL,       KC_A,         KC_S,   KC_E,   KC_T,   KC_G,
+        CTL_T(KC_EQL),  KC_A,         KC_S,   KC_E,   KC_T,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_MINS,
         LT(SYMB,KC_GRV),KC_QUOT,      MO(MDIA),KC_LALT,KC_LGUI,
                                                  KC_LEFT,  KC_RIGHT,
