432c3eb4fe4af308f61c1d3b7e02a4674438e779
Aaron Patterson <aaron.patterson@gmail.com>
2019-10-27 17:52:13 -0700

    something

--- keyboards/ergodox_ez/keymaps/tenderlove/keymap.c
+++ keyboards/ergodox_ez/keymaps/tenderlove/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+
 #include "debug.h"
 #include "action_layer.h"
 #include "version.h"
@@ -46,17 +47,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_D,   KC_F,   KC_K,   KC_EQL,
         CTL_T(KC_EQL),  KC_A,         KC_S,   KC_E,   KC_T,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_MINS,
-        LT(SYMB,KC_GRV),KC_QUOT,      MO(MDIA),KC_LALT,KC_LGUI,
+        LT(SYMB,KC_GRV),MO(MDIA),     MO(MDIA),KC_LALT,KC_LGUI,
                                                  KC_LEFT,  KC_RIGHT,
                                                             KC_HOME,
                                                KC_SPC,KC_ENT,KC_END,
         // right hand
-             KC_RIGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-             LSFT(KC_QUOT),KC_J,   KC_U,   KC_R,   KC_L,   KC_SCLN,          KC_BSLS,
-                           KC_Y,   KC_N,   KC_I,   KC_O,   KC_H,             KC_QUOT,
-             LSFT(KC_SCLN),KC_P,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                  KC_RGUI,KC_RALT,KC_LBRC,KC_RBRC,          LT(SYMB,KC_MINS),
-	     LGUI(KC_LBRC),  LGUI(KC_RBRC),
+             KC_RIGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,      KC_MINS,
+             LSFT(KC_QUOT),KC_J,   KC_U,   KC_R,   KC_L,   KC_SCLN,   KC_BSLS,
+                           KC_Y,   KC_N,   KC_I,   KC_O,   KC_H,      KC_QUOT,
+             LSFT(KC_SCLN),KC_P,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,
+                              LGUI_T(KC_TAB),KC_RALT,KC_LBRC,KC_RBRC,   LT(SYMB,KC_MINS),
+             LGUI(KC_LBRC),  LGUI(KC_RBRC),
              KC_PGUP,
              KC_PGDN,CTL_T(KC_ESC), ALT_T(KC_BSPACE)
     ),
@@ -86,18 +87,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // left hand
        M(0),   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  RESET,
        KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,BL_ON,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_RIGHT,
+       LGUI(KC_LBRC),KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
+       KC_LEFT,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_RIGHT,
        KC_TRNS,EPRM,   KC_TRNS,KC_TRNS,KC_TRNS,
-                                       RGB_MOD,KC_TRNS,
-                                               KC_TRNS,
-                               RGB_VAD,RGB_VAI,KC_TRNS,
+                                       RGB_MOD,RGB_VAD,
+                                               RGB_VAI,
+                               KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        RESET,   KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
        BL_INC,  KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_LEFT, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, LGUI(KC_RBRC),
+       KC_LEFT, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_RIGHT,
+                         KC_COMM,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
        RGB_TOG, RGB_SLD,
        KC_TRNS,
        KC_TRNS, RGB_HUD, RGB_HUI
--- keyboards/planck/keymaps/tenderlove/keymap.c
+++ keyboards/planck/keymaps/tenderlove/keymap.c
@@ -19,9 +19,6 @@ enum planck_layers {
   _NORMAN,
   _SYMB,
   _MOUSE,
-  _LOWER,
-  _RAISE,
-  _PLOVER,
   _ADJUST
 };
 
@@ -29,11 +26,7 @@ enum planck_keycodes {
   NORMAN = SAFE_RANGE,
   SYMB,
   MOUSE,
-  PLOVER,
-  LOWER,
-  RAISE,
   BACKLIT,
-  EXT_PLV,
   BOOTLOADER
 };
 
@@ -55,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_NORMAN] = {
-  {LT(_MOUSE,KC_TAB),KC_Q,    KC_W,    KC_D,    KC_F,          KC_K,    KC_J,    KC_U,          KC_R,    KC_L,    KC_SCLN, KC_BSPC},
+  {LT(_MOUSE,KC_TAB),KC_Q,    KC_W,    KC_D,    KC_F,          KC_K,    KC_J,    KC_U,          KC_R,    KC_L,    KC_SCLN, LT(_ADJUST, KC_BSPC)},
   {CTL_T(KC_EQL),    KC_A,    KC_S,    KC_E,    KC_T,          KC_G,    KC_Y,    KC_N,          KC_I,    KC_O,    KC_H,    KC_QUOT},
   {KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,          KC_B,    KC_P,    KC_M,          KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
   {LT(_SYMB,KC_GRV), BACKLIT,  KC_TAB, KC_LALT, GUI_T(KC_ENT), KC_SPC,  CTL_T(KC_BSPC), KC_ESC, KC_RALT, KC_LBRC, KC_RBRC, LT(_SYMB,KC_MINS)}
@@ -73,10 +66,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_SYMB] = {
-  {_______, KC_EXLM,    KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_UP,   KC_7,    KC_8,   KC_9,  KC_ASTR, KC_LEFT},
-  {KC_ESC,  KC_HASH,    KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  KC_DOWN, KC_4,    KC_5,   KC_6,  KC_PLUS, KC_RIGHT},
-  {KC_LSFT, KC_PERC,    KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_AMPR, KC_1,    KC_2,   KC_3,  KC_BSLS, KC_ENT },
-  {KC_TRNS, BOOTLOADER, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DOT, KC_0,  KC_UP,   KC_TRNS }
+  {_______,        KC_EXLM,    KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_UP,   KC_7,    KC_8,   KC_9,  KC_ASTR, LGUI(KC_LBRC)},
+  {LGUI(KC_LBRC),  KC_HASH,    KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  KC_DOWN, KC_4,    KC_5,   KC_6,  KC_PLUS, LGUI(KC_RBRC)},
+  {KC_LEFT,        KC_PERC,    KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_AMPR, KC_1,    KC_2,   KC_3,  KC_BSLS, KC_RIGHT },
+  {KC_TRNS,        BOOTLOADER, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DOT, KC_0,  KC_UP,   KC_TRNS }
 },
 
 /* Dvorak
@@ -97,6 +90,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R}
 },
 
+[_ADJUST] = LAYOUT_planck_grid(
+    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, _______,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______,  _______,
+    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+)
 
 };
 
@@ -128,7 +127,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         #ifdef AUDIO_ENABLE
           PLAY_NOTE_ARRAY(tone_norman, false, 0);
         #endif
-        persistant_default_layer_set(1UL<<_NORMAN);
+          if (record->event.pressed) {
+              set_single_persistent_default_layer(_NORMAN);
+          }
       }
       return false;
       break;
