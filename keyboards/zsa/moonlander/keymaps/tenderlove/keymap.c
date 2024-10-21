7d3dff9ed00398904291507b56d79b8c9c4833a1
Aaron Patterson <tenderlove@ruby-lang.org>
2024-10-21 12:13:00 -0700

    Fix compilation

--- keyboards/zsa/moonlander/keymaps/tenderlove/keymap.c
+++ keyboards/zsa/moonlander/keymaps/tenderlove/keymap.c
@@ -8,7 +8,7 @@ enum layers {
 };
 
 enum custom_keycodes {
-  EPRM = ML_SAFE_RANGE,
+  EPRM = SAFE_RANGE,
   VRSN,
 };
 
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,          KC_W,           KC_D,            KC_F,           KC_K,    _______,              LSFT(KC_QUOT),  KC_J,     KC_U,              KC_R,            KC_L,          KC_SCLN,       KC_BSLS,
   CTL_T(KC_EQL),  KC_A,          KC_S,           KC_E,            KC_T,           KC_G,    KC_MINS,              LSFT(KC_SCLN),  KC_Y,     KC_N,              KC_I,            KC_O,          KC_H,          KC_QUOT,
         KC_LSFT,  KC_Z,          KC_X,           KC_C,            KC_V,           KC_B,                                          KC_P,     KC_M,              KC_COMM,         KC_DOT,        KC_SLSH,       KC_RSFT,
-LT(SYMB,KC_GRV),  MO(MDIA),      _______,        LALT_T(KC_LEFT), LGUI_T(KC_ENT), _______,                                       _______,  LGUI_T(KC_BSPACE), RALT_T(KC_RGHT), LGUI(KC_LBRC), TD(MDIA_RBRC), LT(SYMB, KC_ESC),
+LT(SYMB,KC_GRV),  MO(MDIA),      _______,        LALT_T(KC_LEFT), LGUI_T(KC_ENT), _______,                                       _______,  LGUI_T(KC_BSPC), RALT_T(KC_RGHT), LGUI(KC_LBRC), TD(MDIA_RBRC), LT(SYMB, KC_ESC),
                                                KC_SPC,  _______, _______,                                        _______, _______, CTL_T(KC_ESC)
     ),
 
@@ -51,61 +51,4 @@ LT(SYMB,KC_GRV),  MO(MDIA),      _______,        LALT_T(KC_LEFT), LGUI_T(KC_ENT)
         _______, _______, _______, _______, _______,          _______,           _______,          KC_VOLD, KC_VOLU, KC_MUTE, _______, _______,
                                             _______, _______, _______,           _______, _______, _______
     ),
-};
-
-typedef struct {
-    bool is_press_action;
-    td_state_t state;
-} tap;
-
-static tap tap_state = {
-    .is_press_action = true,
-    .state = 0
-};
-
-td_state_t
-current_state(qk_tap_dance_state_t *state)
-{
-    if (state->count == 1) {
-        if (!state->pressed) {
-            return SINGLE_TAP;
-        }
-        else {
-            return SINGLE_HOLD;
-        }
-    }
-    else {
-        return TAP_UNKNOWN;
-    }
-}
-
-void
-ql_finished(qk_tap_dance_state_t *state, void *user_data)
-{
-    tap_state.state = current_state(state);
-
-    switch(tap_state.state) {
-        case SINGLE_TAP:
-            tap_code16(LGUI(KC_RBRC));
-            break;
-        case SINGLE_HOLD:
-            layer_on(MDIA);
-            break;
-        default:
-            break;
-    }
-}
-
-void
-ql_reset(qk_tap_dance_state_t *state, void *user_data)
-{
-    if (tap_state.state == SINGLE_HOLD) {
-        layer_off(MDIA);
-    }
-
-    tap_state.state = TAP_NONE;
-}
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [MDIA_RBRC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ql_finished, ql_reset)
 };
